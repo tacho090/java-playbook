795bab9cca532c00b32654b61534f4763ec4eaf8
Pablo Mora <pablo.mora@experian.com>
2025-03-06 21:59:17 -0600

    LocalizationDemo

---
+++ src/main/java/strings/ReverseString.java
@@ -0,0 +1,8 @@
+package strings;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        var builder = new StringBuilder("abcdefgh").reverse();
+        System.out.println(builder);
+    }
+}
